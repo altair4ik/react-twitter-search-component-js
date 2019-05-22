847158e6aa021feb4ed7ae2abccb3e4683fd6efb
altair4ik <shkabara.alexandr@gmail.com>
2019-05-22 09:37:09 +0300

    fix replace linkify hashtags

--- src/helpers/twitter-text.helper.js
+++ src/helpers/twitter-text.helper.js
@@ -2,7 +2,13 @@ export default class TwitterTextHelper {
     static textLinkify = (data) => {
         let str = data.full_text;
         data.entities.hashtags.forEach((item) => {
-            str = str.replace(`#${item.text}`, `<a href="https://twitter.com/hashtag/${item.text}?src=hash">#${item.text}</a>`);
+            const regex = new RegExp(`#${item.text}`, 'g');
+            str = str.replace(regex, (mystr, offset, s) => {
+                if (s[offset + mystr.length] || s[offset + mystr.length] === ' ') {
+                    return `<a href="https://twitter.com/hashtag/${item.text}?src=hash">#${item.text}</a>`;
+                }
+                return mystr;
+            });
         });
         data.entities.urls.forEach((item) => {
             str = str.replace(`${item.url}`, `<a href="${item.url}">${item.url}</a>`);
